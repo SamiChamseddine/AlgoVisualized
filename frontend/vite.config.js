7dbe8518917d1a2fef39acf8ecb2b4b2c6cf0a4c
Zer0pearl <Zer0_pearl313@hotmail.com>
2024-12-29 23:55:31 +0200

    first commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: './frontend', // Set the root directory for your project (if your index.html is in the root of 'frontend')
+  build: {
+    outDir: '../dist', // Output directory for the build
+  },
 })
