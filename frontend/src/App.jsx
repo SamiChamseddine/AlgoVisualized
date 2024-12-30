be4cef4a3ea7d22e17f90312913b66a781aa781b
Zer0pearl <Zer0_pearl313@hotmail.com>
2024-12-30 03:39:32 +0200

    first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,26 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import ProtectedRoute from "./components/ProtectedRoute";
+import SortVisualizer from "./pages/SortVisualizer";  
+
+function Logout() {
+  localStorage.clear();
+  return <Navigate to="/login" />;
+}
+
+function RegisterAndLogout() {
+  localStorage.clear();
+  return <Register />;
+}
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Protected Routes */}
         <Route
           path="/"
           element={
@@ -12,22 +30,18 @@ function App() {
           }
         />
         
-        <Route
+        <Route 
           path="/sort-visualizer"
           element={
             <ProtectedRoute>
               <SortVisualizer />
             </ProtectedRoute>
           }
-        />
-
-        {/* Public Routes */}
+          /> 
         <Route path="/login" element={<Login />} />
         <Route path="/logout" element={<Logout />} />
         <Route path="/register" element={<RegisterAndLogout />} />
-
-        {/* Catch-all for not found */}
-        <Route path="*" element={<NotFound />} />
+        <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </BrowserRouter>
   );
