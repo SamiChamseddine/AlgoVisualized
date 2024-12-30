68148a0c8fed262903df5fdde40e371155d2df75
Zer0pearl <Zer0_pearl313@hotmail.com>
2024-12-30 02:15:17 +0200

    first commit

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,16 +4,20 @@ from api.views import CreateUserView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.shortcuts import render
 
-
+# Serve the React app for all non-API requests
 def serve_react_app(request, path=''):
-    return render(request, 'index.html')
+    return render(request, 'index.html')  # Ensure this is the correct path to your React app's index.html
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # API routes
     path("api/user/register/", CreateUserView.as_view(), name="register"),
     path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
     path("api-auth/", include("rest_framework.urls")),
     path("api/", include("api.urls")),
-    path('<path:path>', serve_react_app),
+    
+    # Serve the React app for all other routes
+    path('<path:path>', serve_react_app),  # This will catch all non-API requests
 ]
