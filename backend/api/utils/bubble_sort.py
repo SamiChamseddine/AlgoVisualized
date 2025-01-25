7925f7484b94302b455b20ff4b63a7199ae57c9e
Zer0pearl <Zer0_pearl313@hotmail.com>
2025-01-25 19:51:06 +0200

    throttling test2

--- backend/api/utils/bubble_sort.py
+++ backend/api/utils/bubble_sort.py
@@ -19,7 +19,7 @@ async def bubble_sort(arr, send_fn, delay, throttle):
                 swaps += 2
 
             
-            if i % throttle == 0:
+            if i % 10 == 0:
                 await send_fn(arr, comparisons, arrayAccesses, swaps, highlighted_indices)
                 await asyncio.sleep(delay)  
 
