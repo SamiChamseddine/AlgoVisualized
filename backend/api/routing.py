0582c4f08a25fa3d6f1ad9a8be985cd0221c1e06
Sami Chamseddine <74697345+SamiChamseddine@users.noreply.github.com>
2025-04-30 13:33:03 +0300

    Delete backend directory

--- backend/api/routing.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .consumers import SortingConsumer, FittingConsumer
-
-websocket_urlpatterns = [
-    path('ws/sort/', SortingConsumer.as_asgi()),
-    path('ws/fit/', FittingConsumer.as_asgi()),
-]
